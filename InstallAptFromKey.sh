2c83340aca6601d38cd001367abdf411c903dc29
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-12-19 14:29:10 +0100

    addded easy installation

---
+++ InstallAptFromKey.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#This has gotten significantly simpler tbh
+sudo wget https://raw.githubusercontent.com/Booklordofthedings/Beefdev-Utils/main/beefdev.sources -p=/etc/apt/sources.list.d
+sudo apt update
