2324b2535a78c625ec221e092ac4afb94ed7cf2b
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-12-22 14:29:32 +0100

    further fixes

--- InstallAptFromKey.sh
+++ InstallAptFromKey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #This has gotten significantly simpler tbh
-sudo wget https://raw.githubusercontent.com/Booklordofthedings/Beefdev-Utils/main/beefdev.sources -p=/etc/apt/sources.list.d
+sudo wget "https://raw.githubusercontent.com/Booklordofthedings/Beefdev-Utils/main/beefdev.sources" -P "/etc/apt/sources.list.d"
 sudo apt update
